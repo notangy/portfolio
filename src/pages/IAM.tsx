781869bc1cd73edc51645ed074433df29eabbda2
notangy <nnotarangelo7@gmail.com>
2025-08-08 20:18:47 +0100

    fix vite reactivity

--- src/pages/IAM.tsx
+++ src/pages/IAM.tsx
@@ -1,28 +1,31 @@
-function IAM() {
+function Iam() {
   return (
     <>
-      <div className="flex h-screen w-screen p-5">
-        <small className="w-1/2 ">Last updated 08/2025</small>
+      <div className="w-full h-full p-2">
+        <small>Last updated 08/2025</small>
         <h1>Who the devil are you?</h1>
-        <p>
-          {" "}
+        <div className="p-2">
+        <p className="m-4">
           Nicole is a mid-level fullstack software engineer with <s>1</s> 3
-          years experience!{" "}
-        </p>
-
-        <p>Her skillset is as follows:</p>
+          years experience!
+          <br />
+         Her skillset is as follows:
+         </p>
         <ul>
           <li>JavaScript/TypeScript</li>
           <li>React</li>
           <li>Svelte</li>
           <li>Node.js</li>
-          <li>Python</li>
+          <li><a href="./snake">Python</a></li>
           <li>PostgreSQL</li>
           <li>HTML/CSS</li>
         </ul>
-      </div>
+        
+</div>
+       
+</div>
     </>
   );
 }
 
-export default IAM;
+export default Iam;
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,39 +2,49 @@ import { createRoot } from "react-dom/client";
 import "../index.css";
 import App from "./App";
 import Navbar from "./Navbar";
-import IAM from "./IAM";
+import SnakeGame from "./snake";
+import Iam from "./Iam";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import type { IconType } from "react-icons/lib";
 import { FaHome, FaFileCode } from "react-icons/fa";
 import { IoPersonCircle } from "react-icons/io5";
 
-interface MenuItem {
+
+export interface MenuItem {
   label: string;
   path: string;
-  icon: IconType;
-  element: React.ReactNode;
+  icon: React.ComponentType<any>;
+  element: React.ComponentType<any>;
 }
 
 export const menuItems: MenuItem[] = [
-  { label: "Home", path: "/", icon: FaHome, element: <App /> },
-  { label: "About", path: "/iam", icon: IoPersonCircle, element: <IAM /> },
-  { label: "Projects", path: "./projects", icon: FaFileCode, element: <App /> },
+  { label: "Home", path: "/", icon: FaHome, element: App },
+  { label: "About", path: "/iam", icon: IoPersonCircle, element: Iam },
+  { label: "Projects", path: "/projects", icon: FaFileCode, element: App },
 ];
 
+const secretRoutes: MenuItem[] = [
+  { label: "Snake Game", path: "/snake", icon: FaFileCode, element: SnakeGame },
+  // Add more secret routes here if needed
+];
+
+let allItems = [...menuItems, ...secretRoutes];
+
 createRoot(document.getElementById("root")!).render(
+   <BrowserRouter>
   <div className="flex h-screen w-screen p-5">
-    <BrowserRouter>
+   
       <div className="w-[200px] ">
         <Navbar />
       </div>
       <div className="w-3/4 ">
         <Routes>
-          {menuItems.map((item) => (
-            <Route key={item.path} path={item.path} element={item.element} />
+          {allItems.map((item) => (
+            <Route key={item.path} path={item.path} element={<item.element />} />
           ))}
         </Routes>
       </div>
-    </BrowserRouter>
-  </div>,
+   
+  </div>
+   </BrowserRouter>
 );
