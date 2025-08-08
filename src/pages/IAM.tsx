05612d11f00594b82b10114606fa405f17da3a2b
notangy <nnotarangelo7@gmail.com>
2025-08-08 20:34:12 +0100

    updated about

--- src/pages/IAM.tsx
+++ src/pages/IAM.tsx
@@ -1,26 +1,37 @@
 function Iam() {
   return (
     <>
-      <div className="w-full h-full p-2">
+      <div className="w-full h-full">
         <small>Last updated 08/2025</small>
         <h1>Who the devil are you?</h1>
         <div className="p-2">
-        <p className="m-4">
-          Nicole is a mid-level fullstack software engineer with <s>1</s> 3
-          years experience!
+        <p className="mt-4">
+          Nicole is a mid-level fullstack software engineer with 3
+          years experience! in the cybersecurity industry!
+
           <br />
          Her skillset is as follows:
          </p>
-        <ul>
+        <ul className="list-disc m-4">
           <li>JavaScript/TypeScript</li>
           <li>React</li>
           <li>Svelte</li>
           <li>Node.js</li>
+          <li>Golang</li>
           <li><a href="./snake">Python</a></li>
           <li>PostgreSQL</li>
           <li>HTML/CSS</li>
         </ul>
         
+
+        <b>Fun facts!</b>
+
+        <ul className="list-disc m-4">
+          <li>She graduated from Abertay Universtiy in 2020 with first class honors in Computing 🎓</li>
+          <li>Her favorite Transformer is Soundwave</li>
+          <li>When she isn't coding, she likes to make custom plushies</li>
+          <li>She is currently learning Java ☕️</li>
+        </ul>
 </div>
        
 </div>
