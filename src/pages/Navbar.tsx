cff7ac22891c264c3d2d216102aee57650f51095
notangy <nnotarangelo7@gmail.com>
2025-08-08 19:53:53 +0100

    working page navigation

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,28 +1,18 @@
-import type { IconType } from "react-icons";
 import "../Navbar.css";
-import { FaHome, FaFileCode } from "react-icons/fa";
-import { IoPersonCircle } from "react-icons/io5";
 import React from "react";
-
-type MenuItem = [string, IconType];
-
-const menuItems: Map<string, MenuItem> = new Map([
-  ["Home", ["/", FaHome]],
-  ["About", ["/iam", IoPersonCircle]],
-  ["Projects", ["/projects", FaFileCode]],
-  ["Tools", ["/tools", FaHome]],
-]);
+import { menuItems } from "./main.tsx"; // Assuming you have a separate file for menu items
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="sidebar items-center flex flex-col">
       <ul>
-        {Array.from(menuItems).map(([name, details]) => (
-          <li key={name} className="flex items-center p-2 hover:bg-grey-700">
-            <a href={details[0]}>
-              {React.createElement(details[1], { className: "mr-2" })}
-              {name}
-            </a>
+        {menuItems.map((item) => (
+          <li key={item.label}>
+            <Link to={item.path}>
+              {React.createElement(item.icon, { className: "mr-2" })}
+              {item.label}
+            </Link>
           </li>
         ))}
       </ul>
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,15 +2,39 @@ import { createRoot } from "react-dom/client";
 import "../index.css";
 import App from "./App";
 import Navbar from "./Navbar";
-import SnakeGame from "../scripts/snake";
+import IAM from "./IAM";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import type { IconType } from "react-icons/lib";
+import { FaHome, FaFileCode } from "react-icons/fa";
+import { IoPersonCircle } from "react-icons/io5";
+
+interface MenuItem {
+  label: string;
+  path: string;
+  icon: IconType;
+  element: React.ReactNode;
+}
+
+export const menuItems: MenuItem[] = [
+  { label: "Home", path: "/", icon: FaHome, element: <App /> },
+  { label: "About", path: "/iam", icon: IoPersonCircle, element: <IAM /> },
+  { label: "Projects", path: "./projects", icon: FaFileCode, element: <App /> },
+];
 
 createRoot(document.getElementById("root")!).render(
   <div className="flex h-screen w-screen p-5">
-    <div className="w-[200px] ">
-      <Navbar />
-    </div>
-    <div className="w-3/4 ">
-      <App />
-    </div>
+    <BrowserRouter>
+      <div className="w-[200px] ">
+        <Navbar />
+      </div>
+      <div className="w-3/4 ">
+        <Routes>
+          {menuItems.map((item) => (
+            <Route key={item.path} path={item.path} element={item.element} />
+          ))}
+        </Routes>
+      </div>
+    </BrowserRouter>
   </div>,
 );
