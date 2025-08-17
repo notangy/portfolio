92c999039615b1e467f044ed5a03e14f3fba98a2
notangy <nnotarangelo7@gmail.com>
2025-08-17 19:28:48 +0100

    more mobile format fixes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,7 +24,7 @@ const typingText: string[] = [
 
 function Home() {
   return (
-    <div className="flex">
+    <div className="flex flex-col md:flex-row items-center md:items-start">
       <div className="w-1/2 ">
         <div className="mb-10">
           <h1>
@@ -42,20 +42,25 @@ function Home() {
           Welcome! This page was made by Nicole to host projects, custom tools
           and other things she wants to show off.
         </p>
-        <p className="flex gap-2">
-          Feel free to poke around using the links up there <LuPointer /> or
-          scroll down if you'd prefer!
+        <p>
+          Feel free to poke around using the links up there
+          <LuPointer
+            size={24}
+            className="inline-block align-text-bottom mx-1"
+          />{" "}
+          or scroll down if you'd prefer!
         </p>
       </div>
 
-      <div className="w-1/2 flex flex-col items-end mr-40">
+      {/* Image Div here */}
+      <div className="w-full md:w-1/2 flex flex-col items-center sm:mt-20 md:items-end md:mr-40 mb-10 md:mb-0">
         <div className="neon-border rounded-full inline-block">
           <img
             src={"https://github.com/notangy.png"}
-            className="rounded-full block"
+            className="rounded-full block md:w-64 md:h-64 lg:w-80 lg:h-80"
           />
         </div>
-        <p className="mt-10 text-center text-sm mr-20">
+        <p className="mt-10 text-center text-sm lg:mr-10">
           Pictured above: my current GitHub avatar
         </p>
       </div>
