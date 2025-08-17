7601cc3a09b13a52d694e1699de6beac5e603868
notangy <nnotarangelo7@gmail.com>
2025-08-17 16:34:46 +0100

    create 404 fallback page from index

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-export default defineConfig({
-  base: "/portfolio",
-  plugins: [tailwindcss(), react()],
+export default defineConfig(({ mode }) => {
+  const plugins = [tailwindcss(), react()];
+
+  if (mode === "production") {
+    plugins.push(
+      viteStaticCopy({
+        targets: [{ src: "index.html", dest: "dists", rename: "404.html" }],
+        hook: "writeBundle",
+      })
+    );
+  }
+
+  return {
+    base: "/portfolio",
+    plugins: plugins,
+  };
 });
